ffe7599a89b15a1fca3097c76786a316cd93fc90
Ankit Kumar <kumar.ankit@cloudanalogy.com>
2024-03-09 23:42:24 +0530

    Solved

--- tests/sdtest.spec.ts
+++ tests/sdtest.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.locator('body').click();
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
